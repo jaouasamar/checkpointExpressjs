4970b1e12e6f4df2e48dcce44591c7e406013683
jaouasamar <jaoua.samar@gmail.com>
2022-02-01 09:55:34 +0100

    second commit

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require ('express')
 const app = express();
 const path = require ('path')
+const PORT=5000
 var today= new Date()
 const myLogger=(req,res,next)=>{
     if(today.getHours()<9||today.getHours()>17||today.getDay()==0||today.getDay()==6)
      res.sendFile((path.join(__dirname,'public','img','closed-sign.png')))
      else
      next()
+    // console.log(today.getDay());
 }
 app.use(myLogger);
 
 app.use(express.static(path.join(__dirname,'public')))
-app.listen(5000,(err)=>{
+app.listen(PORT,(err)=>{
     if(err) {throw err}
     else {console.log('SERVER IS UP AND RUNNING....')}
 })
